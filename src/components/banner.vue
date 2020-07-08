8ee91786fa1f5f99efa04ee8f63451aaf60bae42
unknown <lai.jinwen@vpclub.cn>
2020-07-08 18:54:42 +0800

    首页布局大体

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,35 +1,34 @@
 <template>
-  <van-swipe :autoplay="3000">
-    <van-swipe-item :class="v-swipe-item" v-for="(image, index) in images" :key="index">
-      <img v-lazy="image" />
-    </van-swipe-item>
-  </van-swipe>
+  <div>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item class="van_swipe_item" v-for="(item, index) in bannerData" :key="index">
+        <img v-lazy="item.img" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import { Lazyload } from 'vant';
+import Vue from "vue";
+import { Lazyload } from "vant";
 
 Vue.use(Lazyload);
 export default {
-data() {
+  props: ["bannerData"],
+  data() {
     return {
-      images: [
-        'https://img.yzcdn.cn/vant/apple-1.jpg',
-        'https://img.yzcdn.cn/vant/apple-2.jpg',
-      ],
+      
     };
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.van-swipe-item{
-  img{
+.van_swipe_item {
+  img {
     display: block;
     box-sizing: border-box;
     width: 100%;
-    height: 140px;
     text-align: center;
   }
 }
