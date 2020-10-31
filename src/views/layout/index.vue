f2ced53fac627b2a5d695851d73cb160b8acc931
= <=>
2020-10-31 16:17:23 +0800

    '自动登陆功能'

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -14,9 +14,11 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import MusicPlayer from '../music/components/player/player.vue';
 import FlightService from '../../service/flight';
+import {localStore} from '../../utils/data-management';
 
 @Component({
-	name:'LayoutIndex',
+	name
+	:'LayoutIndex',
 	components:{
 		MusicPlayer
 	}
@@ -24,29 +26,18 @@ import FlightService from '../../service/flight';
 export default class LayoutIndex extends Vue {
   @Prop() private msg!: string;
   
-
-  
-
-  
-  /* isDemo */
-  
  private created(){
-  	
+	  let token = localStore.get('token')||'';
+	  if(token){
+		 // this.autoLogin(token);
+	  }
   }
   
-  private mounted(){
-	  console.log('index:mounted');
-  }
   
-  private beforeDestroy(){
-	  console.log('beforeDestroy');
-  }
-  
-  private getFlightInfo(){
-	  var xhr=new XMLHttpRequest();
+  private autoLogin(token){
+	 /* var xhr=new XMLHttpRequest();
 	   //使用GET方式请求指定网址的页面
-	   xhr.open('GET',process.env.VUE_APP_API_URL + '/v1/api/FlightInfo',false);
-	   //发送空内容请求
+	   xhr.open('GET',process.env.VUE_APP_API_URL + '/user/autoLogin',false);
 	   xhr.send(null);
 	   
 	   if(xhr.status===200){//200状态码表示正常
@@ -54,10 +45,40 @@ export default class LayoutIndex extends Vue {
 			this.$store.dispatch('setFlightInfo',res.data);
 	   }else{
 	  		this.$toast('获取航班信息失败!');
-	   }  
+	   }  */
+	   
+	   var xhr=new XMLHttpRequest();
+	   xhr.open('POST',process.env.VUE_APP_API_URL + '/user/autoLogin',false);
+	   // 添加http头，发送信息至服务器时内容编码类型
+	   //xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	   xhr.setRequestHeader('Content-Type','application/json;charset=UTF-8');
+	   xhr.setRequestHeader('test','haha');
+	 /*  xhr.onreadystatechange=function(){
+	   	console.log('readyState:' + xhr.readyState);
+	       if (xhr.readyState==4){
+	           if (xhr.status==200 || xhr.status==304){
+	               // console.log(xhr.responseText);
+	               fn.call(xhr.responseText);
+	           }
+	       }
+	   }; */
+	   xhr.send(JSON.stringify({token:token}));
+	   if(xhr.status===200){
+		   let res = JSON.parse(xhr.responseText);
+		  if(res.code == '1000'){
+			   this.$store.dispatch('setUserInfo', {
+			       nickname: res.datas.nickName,
+			       token: res.datas.token,
+			   	   phone:res.datas.phone,
+			       userId: res.datas.id
+			     }).then((res: any) => {
+			   	  this.$router.push({
+			   	    name: 'home'
+			   	  });
+			     });
+		   }
+	   }
   }
-  
-  
 }
 </script>
 
